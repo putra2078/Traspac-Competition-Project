5f6e1c280b76cc668ea1b5b02bc6db3e993234f8
Putra <alphaaldiansyah@gmail.com>
2025-11-03 15:44:48 +0700

    hotfix : fix reference key placemenet

--- migrations/000007_create_employees_table.up.sql
+++ migrations/000007_create_employees_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS employees (
     id SERIAL PRIMARY KEY,
-    user_id REFERENCES INTEGER users(id),
+    user_id INTEGER REFERENCES users(id),
     nip VARCHAR(15) NOT NULL UNIQUE,
     status VARCHAR(15) NOT NULL,
-    manager_id REFERENCES INTEGER managers(id),
-    contact_id REFERENCES INTEGER contacts(id),
-    position_id REFERENCES INTEGER positions(id),
-    department_id REFERENCES INTEGER departments(id),
+    manager_id INTEGER REFERENCES managers(id),
+    contact_id INTEGER REFERENCES contacts(id),
+    position_id INTEGER REFERENCES positions(id),
+    department_id INTEGER REFERENCES departments(id),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 )
